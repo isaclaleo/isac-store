81f4dab30326784bc8046ec8d3d284a73da22467
isaclaleo <isac.renovato@librerialeo.com.mx>
2022-10-21 18:02:53 -0500

    property-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'isac-store';
+  name = 'isac';
+  age = 18;
+  myimg = 'https://cdn.wegow.com/media/artists/aurora/aurora-1632412929.693044.2560x1440.jpg';
+  btDisable = true;
+  person ={
+    name: 'fidel',
+    age: 83,
+    avatar: 'https://cdn.wegow.com/media/artists/aurora/aurora-1632412929.693044.2560x1440.jpg'
+  }
 }
